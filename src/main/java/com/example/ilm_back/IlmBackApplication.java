c54e45061d1c15c08793637fcbf72a193625d3f3
Matis <matisleima@gmail.com>
2023-09-15 21:15:30 +0300

    Initial commit

---
+++ src/main/java/com/example/ilm_back/IlmBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.ilm_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlmBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlmBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ilm_back/IlmBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ilm_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlmBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
