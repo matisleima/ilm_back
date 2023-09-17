61c792759e28efa45399c10bbfe7f452aba54d9b
Matis <matisleima@gmail.com>
2023-09-18 00:37:03 +0300

    Get statistics time descending

--- src/main/java/com/example/ilm_back/domain/statistic/StatisticRepository.java
+++ src/main/java/com/example/ilm_back/domain/statistic/StatisticRepository.java
@@ -1,11 +1,12 @@
 package com.example.ilm_back.domain.statistic;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface StatisticRepository extends JpaRepository<Statistic, Integer> {
-    @Query("select s from Statistic s where s.city = ?1 or ?1 = ''")
+    @Query("select s from Statistic s where s.city = ?1 or ?1 = '' order by s.time desc")
     List<Statistic> getWeatherDataBy(String city);
 }
